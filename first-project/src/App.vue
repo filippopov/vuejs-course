d29fc0d3cda74e4c8cc1074415e220d85bfaef21
DESKTOP-FQCLTKM\Popov <popovfilip444@gmail.com>
2019-07-21 22:08:11 +0300

    vue js

---
+++ first-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <timer :message="timerMessage" :number="150" :numbers="[1]">
+            <template v-slot:person="slotProps">
+                <h2>{{slotProps.person.name}} {{slotProps.person.age}}</h2>
+            </template>
+            <template v-slot:message>
+                {{msg}}
+            </template>
+        </timer>
+    </div>
+</template>
+
+
+
+<script>
+import Timer from './components/Timer'
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            msg: 'Hi',
+            name: 'Gosho',
+        }
+    },
+    components: {
+        Timer
+    },
+    computed: {
+        timerMessage() {
+            return 'Hi Popov ' + this.msg;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ first-project/src/components/Timer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <slot name="person" v-bind:person="person"></slot>
+        <slot name="message"></slot>
+        <h1>{{message}}</h1>
+        <p>{{number}}</p>
+        <ul v-if="numbers.length">
+            <li v-for="(number, index) in numbers">{{number}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: 'Pesho',
+                person: {
+                    name: 'Pesho12',
+                    age: 21,
+                }
+            }
+        },
+        props: {
+            message: {
+                type: String,
+                required: true,
+                default: 'Default Message',
+                validator: m => m.length > 10
+            },
+            number: Number,
+            numbers: Array
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
